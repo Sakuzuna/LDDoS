534a310a869a242002fe9b1eca4ce880d8d38fa7
Sakuzuna <nikita.kopychev@s.wingateschool.com>
2025-02-21 22:22:41 +0000

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,40 @@
 const express = require('express');
+const app = express();
 const path = require('path');
 const attack = require('./attack');
 
-const app = express();
 const PORT = process.env.PORT || 5252;
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+let isAttackRunning = false;
 
 app.get('/attack', (req, res) => {
     const { target, delay, kbSize, method } = req.query;
     const delayMs = delay === 'Z' ? 0 : parseInt(delay, 10);
     const kb = parseInt(kbSize, 10);
-    attack.start(target, delayMs, kb, method);
-    res.send('Attack started!');
+
+    if (!isAttackRunning) {
+        isAttackRunning = true;
+        attack.start(target, delayMs, kb, method);
+        res.send('Attack started!');
+    } else {
+        res.send('Attack is already running.');
+    }
+});
+
+app.get('/stop-attack', (req, res) => {
+    if (isAttackRunning) {
+        isAttackRunning = false;
+        attack.stop(); 
+        res.send('Attack stopped!');
+    } else {
+        res.send('No attack is running.');
+    }
+});
+
+app.get('/attack-status', (req, res) => {
+    res.send(isAttackRunning ? 'Attack is running...' : 'Attack is stopped.');
 });
 
 app.listen(PORT, () => {
